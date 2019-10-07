95cfdad9de755684a981c1d8dee832d4403720bc
Maria Obaturova <m.obaturova@gmail.com>
2019-10-06 23:53:55 -0400

    added a file with lecture + got rid of long comments

--- cpp-memory/addressOf.cpp
+++ cpp-memory/addressOf.cpp
@@ -19,9 +19,4 @@ int main() {
 
 // Output:
 // Value: 7
-// * exactly the large number!
 // Address: 0x7ffeef9eb858
-
-
-/* So, given the length of this number, that's 12 or 13 hexadecimal digits long, we know that this is a very high memory address, which is exactly what we expect of stack memory. Let's look at why this is a high memory address and understand exactly how the memory works in our computer. */
-//As mentioned, stack memory is the default memory for all variables in C++ to be placed. So, we haven't done anything special. We know our variable is going to be in stack memory. Stack memory is a specifically associated with the current function, and the lifecycle of this memory is the lifecycle of the function. So, if you clear an integer inside of the function, that integer exists as long as that function is still running. When the function finally ends or the function finally returns, then the stack memory for that function is going to be released back to the system.
